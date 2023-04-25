64ca71fe63405acdf3f14c0cc0c7acf529a55bd5
Julnbul <julnbul@gmail.com>
2023-04-25 11:34:02 +0300

    new

--- js/script.js
+++ js/script.js
@@ -1,2 +1,17 @@
 'use strict'
 
+const iconMenu = document.querySelector('.header__burger-button');
+const menuBody = document.querySelector('.header__menu');
+const menuPlug = document.querySelector('.menu__plug');
+
+
+//бургер
+if (iconMenu) {
+  iconMenu.addEventListener('click', function (e) {
+    document.body.classList.toggle('_lock');
+    iconMenu.classList.toggle('_active');
+    menuBody.classList.toggle('_active');
+    menuPlug.classList.toggle('_active');
+    menuBody.classList.toggle('hidden-mobile');
+  });
+}
